0fc3ec7546ff22d9d4ae715f8c3329ead5489068
Antonio Carrasco <carrasco.antonio.233@gmail.com>
2025-03-25 11:20:04 +0100

    Demo

--- blackjackspring5/src/main/java/itacademy/blackjackspring5/repository/mongodb/GameRepository.java
+++ blackjackspring5/src/main/java/itacademy/blackjackspring5/repository/mongodb/GameRepository.java
@@ -3,6 +3,7 @@ package itacademy.blackjackspring5.repository.mongodb;
 import itacademy.blackjackspring5.model.mongodb.Game;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface GameRepository extends ReactiveMongoRepository<Game, String> {
